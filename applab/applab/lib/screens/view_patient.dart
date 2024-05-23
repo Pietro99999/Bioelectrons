54b382e752525aea5d171fd6731bf59c1c75e4c3
GaiaB08 <gaia.bortolussi@studenti.unipd.it>
2024-05-23 09:40:33 +0200

    Update view_patient.dart

--- applab/applab/lib/screens/view_patient.dart
+++ applab/applab/lib/screens/view_patient.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-import 'package:prova_homepagepaz/models/modifypatient.dart';
+import 'package:applab/models/modifypatient.dart';
 
 class PatientHome extends StatelessWidget {
    final int patientIndex;
