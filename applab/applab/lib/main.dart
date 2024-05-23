d6d629c834a0a1f9a3b483c13dc2a92ae303ca6b
GaiaB08 <gaia.bortolussi@studenti.unipd.it>
2024-05-23 09:37:20 +0200

    Update main.dart

--- applab/applab/lib/main.dart
+++ applab/applab/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:applab/screens/splashscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:prova_homepagepaz/screens/homepage.dart';
-import 'package:prova_homepagepaz/models/modifypatient.dart';
+import 'package:applab/screens/homepage.dart';
+import 'package:applab/models/modifypatient.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    //Here we are injecting an instance of the MealDB thorugh the widget tree
+    
     return ChangeNotifierProvider<ModifyPatient>(
       create: (context) => ModifyPatient(),
       child: MaterialApp(
