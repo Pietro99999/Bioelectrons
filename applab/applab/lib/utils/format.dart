e075a2672e5cc17310f00cdaa55ff5210f882287
GaiaB08 <gaia.bortolussi@studenti.unipd.it>
2024-05-23 09:36:14 +0200

    Update format.dart

--- applab/applab/lib/utils/format.dart
+++ applab/applab/lib/utils/format.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:prova_homepagepaz/models/patient.dart';
+import 'package:applab/screens/homepage.dart';
 
 
 ///Class that implements a custom [StatelessWidget] that acts as a separator in a [Form].
